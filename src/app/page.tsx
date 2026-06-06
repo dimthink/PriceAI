a81168661790195025f5aedfbdcde55a7188e569
physics-dimension <210272875+physics-dimension@users.noreply.github.com>
2026-06-06 18:52:26 +0800

    Preload homepage explorer data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import { PriceExplorer } from "@/components/PriceExplorer";
 import { SubmissionFloater } from "@/components/SubmissionFloater";
+import { getExplorerData } from "@/lib/data";
+
+export const revalidate = 300;
 
 export default async function Home() {
+  const data = await getExplorerData();
+
   return (
     <>
-      <PriceExplorer restoreStateFromUrl />
+      <PriceExplorer data={data} restoreStateFromUrl />
       <SubmissionFloater />
     </>
   );
